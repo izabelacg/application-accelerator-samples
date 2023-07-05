86a960994656db03e6fbd938fbcb4ef8a0236819
Izabela Gomes <igomes@vmware.com>
2023-07-05 15:15:57 -0400

    greeting from env property

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private Environment environment;
+
+	String getGreeting_FromEnvironment() {
+		return environment.getProperty("greeting");
+	}
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		String greeting = "Spring Boot + Tanzu";
+		if (!getGreeting_FromEnvironment().isEmpty()) {
+			greeting = getGreeting_FromEnvironment();
+		}
+		return String.format("Greetings from %s!", greeting);
 	}
 
 }
